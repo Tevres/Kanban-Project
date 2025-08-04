23e02ba12352d0f7a8e32d9f4444778d33e4a88a
Tevres <daniel.degenstein@outlook.de>
2025-08-04 20:46:56 +0200

    you can add tasks now

---
+++ script.js
@@ -0,0 +1,23 @@
+// get all columns
+const columnsContainer = document.querySelector(".columns");
+
+// function if button is clicked
+function handleAdd(event) {
+  // html element that is clicked, search for button data-add
+  const button = event.target.closest("button[data-add]");
+  if (!button) return;
+
+  // search for column div, to which the button belongs to
+  const column = button.closest(".column");
+  const tasksContainer = column.querySelector(".tasks");
+
+  // create a new task element
+  const task = document.createElement("div");
+  task.className = "task";
+  task.textContent = "Neue Aufgabe";
+
+  tasksContainer.appendChild(task);
+}
+
+// every time button is clicked, run function
+columnsContainer.addEventListener("click", handleAdd);
